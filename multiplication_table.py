a88a90af8b54332e9b4c8fa94f424d0a3409a9ae
Ilya <ilya00004@gmail.com>
2020-08-09 23:54:19 +0400

    added interleaners

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,6 +1,10 @@
-# version 0.2
+# version 0.3
+def interleaner():
+     print("+" + "---+" * 9)
 
 for i in range(1, 10):
+    interleaner()
+    print("|", end="")
     for j in range(1, 10):
         x = i * j
         if x < 10 :
@@ -9,3 +13,4 @@ for i in range(1, 10):
             print(" ", end="")
         print(x, end="|")
     print()
+interleaner()
