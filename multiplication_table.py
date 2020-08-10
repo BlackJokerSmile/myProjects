e0d510403437840693f84fea993fea0215ec1e24
Ilya <ilya00004@gmail.com>
2020-08-11 00:31:50 +0400

    Did 10*10 table this the x and y columns and lines

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,14 +1,27 @@
-# version 0.3
+# version 0.4
 def interleaner():
-     print("+" + "---+" * 9)
+     print("+" + "---+" * 11)
 
-for i in range(1, 10):
+def multiply(x, y):
+    if x == 0 :
+        return y
+    elif y == 0:
+        return x
+    else:
+        return x * y
+
+for c in range(0, 11):
     interleaner()
     print("|", end="")
-    for j in range(1, 10):
-        x = i * j
+    for l in range(0, 11):
+        x = multiply(c, l)
+        if x == 0:
+            print("   |", end="")
+            continue
         if x < 10 :
             print("  ", end="")
+        elif x == 100:
+            print("", end="")
         else:
             print(" ", end="")
         print(x, end="|")
